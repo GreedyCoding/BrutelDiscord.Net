50df95975085e2037d1db1d7a754a7da6fcc8690
GreedyCoding <marco_hofer2012@outlook.com>
2019-06-20 22:28:29 +0200

    Added Console Project to the repository

---
+++ BrutelDiscord.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BrutelDiscord.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
