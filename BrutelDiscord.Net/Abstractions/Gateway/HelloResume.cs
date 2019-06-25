53ad923a7b3e38fa1cefbc90d0a9feaeeb548cba
GreedyCoding <marco_hofer2012@outlook.com>
2019-06-25 22:28:02 +0200

    Started adding abstractions of the Payload objects

---
+++ BrutelDiscord.Net/Abstractions/Gateway/HelloResume.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutelDiscord.Abstractions.Gateway
+{
+    class HelloResume
+    {
+        [JsonProperty("heartbeat_interval")]
+        public int HeartbeatInterval { get; set; }
+
+        [JsonProperty("_trace")]
+        public IReadOnlyList<string> Trace { get; private set; }
+    }
+}
---
+++ BrutelDiscord.Net/Abstractions/Gateway/Payload.cs
@@ -0,0 +1,23 @@
+﻿using BrutelDiscord.Enums;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutelDiscord.Abstractions.Gateway
+{
+    class Payload
+    {
+        [JsonProperty("op")]
+        public OpCodes Opcode { get; set; }
+
+        [JsonProperty("d")]
+        public object Data { get; set; }
+
+        [JsonProperty("s", NullValueHandling = NullValueHandling.Ignore)]
+        public int? Sequence { get; set; }
+
+        [JsonProperty("t", NullValueHandling = NullValueHandling.Ignore)]
+        public string EventName { get; set; }
+    }
+}
