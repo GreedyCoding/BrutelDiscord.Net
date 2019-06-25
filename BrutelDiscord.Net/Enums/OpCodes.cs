998d118e549efa2788a9355a6191baf8e6ac5cb9
GreedyCoding <marco_hofer2012@outlook.com>
2019-06-25 22:27:10 +0200

    Added Enumerator for the OpCodes

---
+++ BrutelDiscord.Net/Enums/OpCodes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutelDiscord.Enums
+{
+    public enum OpCodes
+    {
+        Dispatch = 0,
+
+        Heartbeat = 1,
+
+        Identity = 2,
+
+        StatusUpdate = 3,
+
+        VoiceStateUpdate = 4,
+
+        Resume = 6,
+
+        Reconnect = 7,
+
+        RequestGuildMembers = 8,
+
+        InvalidSession = 9,
+
+        Hello = 10,
+
+        HeartbeatAcknowledge = 11
+    }
+}
