70a1f627ea551c9dfccc9ced19bad4d82bf5ba53
GreedyCoding <marco_hofer2012@outlook.com>
2019-07-08 00:15:01 +0200

    Created abstraction for Discord users

---
+++ BrutelDiscord.Net/src/Abstractions/DiscordUser.cs
@@ -0,0 +1,44 @@
+﻿using BrutelDiscord.src.Enums;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutelDiscord.Abstractions
+{
+    /// <summary>
+    /// Represensts a Discord user
+    /// </summary>
+    class DiscordUser
+    {
+        [JsonProperty("id")]
+        public ulong Id { get; internal set; }
+
+        [JsonProperty("username", NullValueHandling = NullValueHandling.Ignore)]
+        public string Username { get; internal set; }
+
+        [JsonProperty("discriminator", NullValueHandling = NullValueHandling.Ignore)]
+        internal string Discriminator { get; set; }
+
+        [JsonProperty("avatar", NullValueHandling = NullValueHandling.Ignore)]
+        public string AvatarHash { get; internal set; }
+
+        [JsonProperty("bot", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsBot { get; internal set; }
+
+        [JsonProperty("mfa_enabled", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? MfaEnabled { get; internal set; }
+
+        [JsonProperty("verified", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? Verified { get; internal set; }
+
+        [JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
+        public string Email { get; internal set; }
+
+        [JsonProperty("premium_type", NullValueHandling = NullValueHandling.Ignore)]
+        public PremiumType? PremiumType { get; internal set; }
+
+        [JsonProperty("locale", NullValueHandling = NullValueHandling.Ignore)]
+        public string Locale { get; internal set; }
+    }
+}
