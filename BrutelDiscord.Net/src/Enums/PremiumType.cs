82d77af126f70bf9b6efb764e3ef222d0a92b124
GreedyCoding <marco_hofer2012@outlook.com>
2019-07-08 00:17:52 +0200

    Cretead enumerator for PremiumTypes

---
+++ BrutelDiscord.Net/src/Enums/PremiumType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutelDiscord.src.Enums
+{
+    public enum PremiumType
+    {
+        NitroClassic = 1,
+
+        Nitro = 2
+    }
+}
