621eb0b120946dcfb25066bfa5c6cafc61c6a3bf
GreedyCoding <marco_hofer2012@outlook.com>
2019-07-11 19:44:11 +0200

    Added enum for channel types and renamed premium types

---
+++ BrutelDiscord.Net/src/Enums/ChannelTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutelDiscord.Enums
+{
+    public enum ChannelTypes
+    {
+        Text = 0,
+
+        Private = 1,
+
+        Voice = 2,
+
+        Group = 3,
+
+        Category = 4,
+
+        Unknown = int.MaxValue
+    }
+}
--- BrutelDiscord.Net/src/Enums/PremiumTypes.cs
+++ BrutelDiscord.Net/src/Enums/PremiumTypes.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BrutelDiscord.src.Enums
+namespace BrutelDiscord.Enums
 {
-    public enum PremiumType
+    public enum PremiumTypes
     {
         NitroClassic = 1,
 
