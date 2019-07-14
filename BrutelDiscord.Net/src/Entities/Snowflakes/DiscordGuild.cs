9eb3588674be8c1977ecc095b1543e624f25eb4a
GreedyCoding <marco_hofer2012@outlook.com>
2019-07-14 23:33:20 +0200

    Starated implementation of DiscordGuild and User Entities

--- BrutelDiscord.Net/src/Entities/Snowflakes/DiscordGuild.cs
+++ BrutelDiscord.Net/src/Entities/Snowflakes/DiscordGuild.cs
@@ -1,9 +1,49 @@
 ﻿using BrutelDiscord.Entities;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace BrutelDiscord.Entities.Snowflakes
 { 
     public class DiscordGuild : SnowflakeObject
     {
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; internal set; }
+
+        [JsonProperty("icon", NullValueHandling = NullValueHandling.Ignore)]
+        public string IconHash { get; internal set; }
+
+        [JsonIgnore]
+        public string IconUrl
+            => !string.IsNullOrWhiteSpace(this.IconHash) 
+            ? $"https://cdn.discordapp.com/icons/{this.Id.ToString(CultureInfo.InvariantCulture)}/{IconHash}.jpg" 
+            : null;
+
+        [JsonProperty("splash", NullValueHandling = NullValueHandling.Ignore)]
+        public string SplashHash { get; internal set; }
+
+        [JsonIgnore]
+        public string SplashUrl
+            => !string.IsNullOrWhiteSpace(this.SplashHash) ? $"https://cdn.discordapp.com/splashes/{this.Id.ToString(CultureInfo.InvariantCulture)}/{SplashHash}.jpg" : null;
+
+        [JsonProperty("owner_id", NullValueHandling = NullValueHandling.Ignore)]
+        internal ulong OwnerId { get; set; }
+
+
+    //"region": "us-east",
+    //"afk_channel_id": "42072017402331136",
+    //"afk_timeout": 300,
+    //"embed_enabled": true,
+    //"embed_channel_id": "41771983444115456",
+    //"verification_level": 1,
+    //"default_message_notifications": 0,
+    //"explicit_content_filter": 0,
+    //"mfa_level": 0,
+    //"widget_enabled": false,
+    //"widget_channel_id": "41771983423143937",
+    //"roles": [],
+    //"emojis": [],
+    //"features": ["INVITE_SPLASH"],
+    //"unavailable": false
 
     }
 }
--- BrutelDiscord.Net/src/Entities/Snowflakes/DiscordUser.cs
+++ BrutelDiscord.Net/src/Entities/Snowflakes/DiscordUser.cs
@@ -6,6 +6,13 @@ namespace BrutelDiscord.Entities.Snowflakes
 {
     class DiscordUser : SnowflakeObject
     {
-
+        //Example User
+        //"username": "Nelly",
+        //"discriminator": "1337",
+        //"avatar": "8342729096ea3675442027381ff50dfe",
+        //"verified": true,
+        //"email": "nelly@discordapp.com",
+        //"flags": 64,
+        //"premium_type": 1
     }
 }
